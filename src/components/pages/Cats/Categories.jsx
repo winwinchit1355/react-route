b9fe50471275ce2110a37beae5fc402dffada638
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-09-23 20:41:59 +0630

    useSearchParams

--- src/components/pages/Cats/Categories.jsx
+++ src/components/pages/Cats/Categories.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export const Categories = () => {
+  const [searchParams,setSearchParams] = useSearchParams();
+  const name = searchParams.get('name');
+  const age = searchParams.get('age');
   return (
     <div>
       <h1>Category Lists</h1>
+      <p>{name} is {age} years old.</p>
+      <button onClick={()=>setSearchParams({name:'Mg Mg',age:34})}>Set Params</button>
+      <button onClick={()=>setSearchParams()}>Reset Params</button>
     </div>
   )
 }
